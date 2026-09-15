4b8a27eac097b32e161da35704d1ba40b4cba22e
Krishan Kant <krishankant882005@gmail.com>
2026-09-16 01:22:11 +0530

    If Else If Code

---
+++ 3.cpp
@@ -0,0 +1,22 @@
+  //  use of if else if staement.  
+  #include <iostream>
+  using namespace std;
+  int main() {
+    int marks;
+    cout << "Enter Your marks: " << endl;
+    cin >> marks;
+    if(marks >= 90) {
+        cout << "A+" << endl;
+    } else if(marks >= 80) {
+        cout << "A" << endl;
+    } else if(marks >= 70) {
+        cout << "B" << endl;
+    } else if(marks >= 60) {
+        cout << "C" << endl;
+    } else if(marks >= 50) {
+        cout << "D" << endl;
+    } else {
+        cout << "F" << endl;
+    }
+    return 0;
+  }
