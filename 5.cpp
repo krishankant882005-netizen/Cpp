6c11459502faaaa3729b0c101a096017b2648f0e
Krishan Kant <krishankant882005@gmail.com>
2026-09-16 23:44:44 +0530

    Switch Case

---
+++ 5.cpp
@@ -0,0 +1,35 @@
+// Switch Case
+#include <iostream>
+using namespace std;
+int main() {
+    int day;
+    cout << "Enter day number: " << endl;
+    cin >> day;
+    switch(day) {
+        case 1: 
+        cout << "Sunday" << endl;
+        break;
+         case 2: 
+        cout << "Monday" << endl;
+        break;
+         case 3: 
+        cout << "Tuesday" << endl;
+        break;
+         case 4: 
+        cout << "Wednesday" << endl;
+        break;
+         case 5: 
+        cout << "Thursday" << endl;
+        break;
+         case 6: 
+        cout << "Friday" << endl;
+        break;
+         case 7: 
+        cout << "Saturday" << endl;
+        break;
+        default:
+        cout << "Invalid";
+        
+    }
+    return 0;
+}
