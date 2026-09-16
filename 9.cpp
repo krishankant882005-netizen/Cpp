d98c64a1eef235a5fbbb672965b6774956dc2d02
Krishan Kant <krishankant882005@gmail.com>
2026-09-17 02:06:55 +0530

    While Loop

---
+++ 9.cpp
@@ -0,0 +1,13 @@
+// while loop
+#include <iostream>
+using namespace std;
+int main() {
+    int sum = 0;
+    int i = 0;
+    while(i<=10) {
+        sum +=i;
+        i++;
+    }
+    cout << sum << endl;
+    return 0;
+}
